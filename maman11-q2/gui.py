62a34881d5d8a45f9f3db0260cced9c5e18e33a5
3-kids-wearing-a-trenchcoat <d.spector1996@gmail.com>
2025-11-12 18:08:07 +0200

    .

---
+++ maman11-q2/gui.py
@@ -0,0 +1,7 @@
+from __future__ import annotations
+from numba import njit
+import numpy as np
+import pyqtgraph as pg
+from pyqtgraph.Qt import QtWidgets, QtCore, QtGui
+import pyqtgraph.opengl as gl
+
