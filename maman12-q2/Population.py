224507d7961fc0d706164a12321b0682cf46478d
3-kids-wearing-a-trenchcoat <d.spector1996@gmail.com>
2025-12-03 12:27:35 +0200

    Added stop conditions to the Population iterator

--- maman12-q2/Population.py
+++ maman12-q2/Population.py
@@ -15,17 +15,31 @@ DEFAULT_INIT_POP_SIGMA = 0.7 # default standard deviation for random values of i
 DEFAULT_CANDIDATE_NUM = 3 # default number of candidates in each tournament for the purpose of procreation
 DEFAULT_CARRY_OVER_NUM = 2 # default number of the best candidates which will be carried over to the next generation
 THREADS = 4 # number of workers in ThreadPoolExecutor
+DEFAULT_MAX_ITER = 10000 # default number of iterations at which we stop regardless of other stop conditions
+DEFAULT_SATISFACTORY = 1e-3 # default satisfactory fitness value, if an individual is less-than-or-equal to it, stop.
+DEFAULT_STAGNATION_LIMIT = 200 # default number of iterations at which, if no significant improvement was found, stop.
+DEFAULT_STAGNATION_DIFF = 1e-3 # default value of difference between iteration best-fitness-score at which we consider
+                               # this generation to be "stagnant" (negligible improvement)
 
 class Population:
     """This class represents the entire population of candidate solution.
     The class contains a list of Individual objects and handles generation of the next generation."""
 
-    def __init__(self, pop_size:int = DEFAULT_POP_SIZE, win_prob:float = DEFAULT_WIN_PROB,
+    def __init__(self, max_iter:int = DEFAULT_MAX_ITER, satisfactory:float = DEFAULT_SATISFACTORY,
+                 stagnation_limit:float = DEFAULT_STAGNATION_LIMIT, stagnation_diff:float = DEFAULT_STAGNATION_DIFF,
+                 pop_size:int = DEFAULT_POP_SIZE, win_prob:float = DEFAULT_WIN_PROB,
                  init_sigma:float = DEFAULT_INIT_POP_SIGMA, tournament_participants:int = DEFAULT_CANDIDATE_NUM,
                  carry_over:int = DEFAULT_CARRY_OVER_NUM):
         """
         Initialize a Population object with Individuals whose values are random. (normally distributed)
         Population is assumed to be initialized AFTER Individual.Individual.set_static_vars has been called.
+        :param max_iter: maximum number of iterations to run (default DEFAULT_MAX_ITER)
+        :param satisfactory: if the best individual has a score less-than-or-equal to this, stop and return it.
+                             (default: DEFAULT_SATISFACTORY)
+        :param stagnation_limit: number of consecutive "stagnant" iterations at which we stop the run.
+                                 (default: DEFAULT_STAGNATION_LIMIT)
+        :param stagnation_diff: a generation whose best fitness improvement is below this value is considered "stagnant"
+                                (default: DEFAULT_STAGNATION_DIFF)
         :param pop_size: total size of population (default: DEFAULT_POP_SIZE)
         :param win_prob: probability of the best current candidate in a tournament to win it. (default: DEFAULT_WIN_PROB)
         :param init_sigma: standard deviation of random values of initial population. (default: DEFAULT_INIT_POP_SIGMA)
@@ -33,6 +47,12 @@ class Population:
         :param carry_over: This number of the Individuals with the best fitness score will carry over to the next generation
                             (default: DEFAULT_CARRY_OVER_NUM)
         """
+        self.max_iter = max_iter
+        self.current_iter = 0 # iteration counter
+        self.satisfactory = satisfactory # satisfactory distance from ideal at which we stop
+        self.stagnation_limit = stagnation_limit # this many stagnant iterations would stop the run
+        self.stagnation_diff = stagnation_diff # improvement below this value will be considered a stagnant iteration
+        self.current_stagnant_iter = 0 # number of consecutive, stagnant iterations
         self.rng = Individual.rng # shared random number generator
         self.pop_size = pop_size # population size
         self.win_prob = win_prob # probability to win a tournament
@@ -54,6 +74,10 @@ class Population:
         # when either one is called we will simply find the carry-overs and store the results for when the other one
         # needs them, instead of running that search twice
         self.carry_over = None
+        # pocket Individual - the best Individual encountered so far in all iterations that have come so far
+        # it is replaced by the current iteration's best whenever its fitness value is better than the current pocket
+        # (I already examine the best current Individual every iteration, so this is not noticeably more expensive)
+        self.pocket: Individual|None = None
 
     def tournament_selection(self, candidates:NDArray[Individual], exclude:Individual=None) -> Individual:
         """
@@ -130,12 +154,19 @@ class Population:
 
     def __next__(self) -> Population:
         """Generate the next generation of the population"""
-        # TODO: implement stop conditions for maximum iterations, stagnation and sufficient fitness
+        # stop if any of the stop conditions are met
+        if (self.current_iter >= self.max_iter or  # max iterations reached
+            (self.pocket is not None and self.pocket.get_fitness_score() <= self.satisfactory) or # satisfactory score
+            self.current_stagnant_iter >= self.stagnation_limit): # reached stagnant iteration limit
+            raise StopIteration
         # TODO: play around with parallelization to make sure it's beneficial and if so, find a conservative value for it
+
+        # initialize next generation
         output = copy.copy(self) # other than pop, all other attributes are identical and can be shallow copies
         output.pop = [] # start with an empty pop
         output.carry_over = [] # carry_over also needs to be reset as the old values aren't relevant
 
+        # generate individuals in the next generation
         children_per_worker_base, children_per_worker_rem = divmod(self.pop_size - self.num_carry_over, THREADS)
         # number of children each worker will generate, divided as equally possible
         child_quant = [children_per_worker_base + 1 if i < children_per_worker_rem
@@ -147,6 +178,20 @@ class Population:
         for fut in futures:
             output.pop += fut.result()
         # add carry-overs to next pop
-        output.pop += self.get_carry_overs()
+        carry_overs = self.get_carry_overs()
+        output.pop += carry_overs
+
+        # update stop-condition-related variables in output
+        output.current_iter += 1 # increase iteration counter
+        pocket_candidate = output.get_best() # get the best fitness Individual in the new generation
+        # check if the next generation is stagnant by checking if the diff of the best fitness scores is less
+        # than or equal to the defined stagnation_diff
+        if np.absolute(carry_overs[0].fitness_score - pocket_candidate.fitness_score) <= self.stagnation_diff:
+            output.current_stagnant_iter += 1 # if so, increase stagnation counter by 1
+        else:
+            output.current_stagnant_iter = 0 # otherwise, this generation is not stagnant and the counter is zeroed
+        # pocket
+        if pocket_candidate < output.pocket: # if new best Individual is better than the pocket individual
+            output.pocket = pocket_candidate # make it the new pocket Individual
 
         return output
