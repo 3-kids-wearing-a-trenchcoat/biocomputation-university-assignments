bae7c91e70d636c6bbb6bbe04c054c47b21bee93
3-kids-wearing-a-trenchcoat <d.spector1996@gmail.com>
2025-12-04 13:21:09 +0200

    Implemented run

---
+++ maman12-q2/RNASeqDeconvolution.py
@@ -0,0 +1,52 @@
+from __future__ import annotations
+import numpy as np
+from numpy.typing import NDArray
+from tqdm.auto import tqdm
+from Individual import Individual, FTYPE
+from Population import Population
+
+class RNASeqDeconvolution:
+    """This class runs the RNA-seq deconvolution genetic algorithm and produces and stores the associated results.
+    In addition to the actual matrix representing the percent of cell types for a sample, these results include:
+    * History of the best fitness score per iteration
+    * History of the worst fitness score per iteration
+    * Confusion matrix compared to the true result (as supplied by input)"""
+    def __init__(self, pop:Population, true_result:NDArray[FTYPE]):
+        """
+        Initialize an RNA-sequence deconvolution run
+        :param pop: Initialized Population object
+        :param true_result: Matrix representing true result, against which the algorithm's result is compared
+        """
+        self.best_history = [] # history of best fitness value per iteration
+        self.worst_history = [] # history of worst fitness value per iteration
+        self.pop = pop # Population object that implements the actual algorithm using Individual
+        self.true_result = true_result # the True result against which this result will be compared
+        self.result: NDArray[FTYPE]|None = None # result at the end of the run (phenotype of pocket Individual)
+        self.result_fitness_score: float|None = None
+        self.confusion_matrix: NDArray[FTYPE]|None = None # confusion matrix of the result compared to true result
+
+    @staticmethod
+    def print_stop_reason(generation:Population) -> None:
+        output = "Stopped: reached "
+        if generation.current_iter >= generation.max_iter:
+            output += "iteration limit"
+        elif generation.current_stagnant_iter >= generation.stagnation_limit:
+            output += str(generation.stagnation_limit) + " consecutive stagnant iterations"
+        else:
+            output += "satisfactory fitness score"
+        print(output)
+
+    def run(self) -> None:
+        """Run the RNA-seq deconvolution algorithm on the given population.
+        This function doesn't return anything and its results need to be extracted with the getter methods"""
+        pbar = tqdm(self.pop, total=self.pop.max_iter, desc="running RNA-seq deconvolution", dynamic_ncols=True)
+        for generation in pbar:
+            self.best_history.append(generation.best_score())
+            self.worst_history.append(generation.worst_fitness_score)
+            pocket_score = generation.pocket.fitness_score
+            pbar.set_postfix_str(f"best score: {self.best_history[-1]:.4f}, "
+                                 f"worst_score: {self.worst_history[-1]:.4f}, pocket score: {pocket_score}:.4f")
+        self.print_stop_reason(generation)
+        self.result = generation.get_pocket().get_phenotype()
+        self.result_fitness_score = generation.result.get_fitness_score()
+
