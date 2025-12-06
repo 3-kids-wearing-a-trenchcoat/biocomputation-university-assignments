876fca2434e0d8e58d5cf1cbf9a3bbbf2fd31e76
3-kids-wearing-a-trenchcoat <d.spector1996@gmail.com>
2025-12-06 15:30:54 +0200

    Implemented compare_to_true_results

--- maman12-q2/main.py
+++ maman12-q2/main.py
@@ -14,13 +14,13 @@ DEFAULT_PARAMS = {"rng_seed": 123,
                   "mut_prob": 0.003,
                   "mut_standard_deviation": 0.55,
                   "crossover_prob": 0.9,
-                  "max_iter": 5000,
+                  "max_iter": 3000,
                   # "max_iter": 999999999,
                   "satisfactory": 1e-3,
-                  "stagnation_limit": 300,
+                  "stagnation_limit": 200,
                   # "stagnation_limit": 99999,
-                  "stagnation_diff": 1e-9,
-                  "pop_size": 200,
+                  "stagnation_diff": 1e-3,
+                  "pop_size": 300,
                   "win_prob": 0.7,
                   "init_sigma": 0.7,
                   "tournament_participants": 3,
@@ -31,7 +31,7 @@ DEFAULT_PARAMS = {"rng_seed": 123,
                   }
 RESULT_LABELS = ["fitness score", "iterations", "cause of stop"]
 
-def parse_input_matrix(path:Path, t:DTypeLike = FTYPE) -> NDArray:
+def parse_input_matrix(path: Path, t: DTypeLike = FTYPE) -> NDArray:
     """
     turn the matrix in the given path into a numpy matrix, discarding row/column names
     :param path: path to file (using pathlib)
@@ -47,7 +47,7 @@ M = parse_input_matrix(DEFAULT_PARAMS.get("M_path"))
 H = parse_input_matrix(DEFAULT_PARAMS.get("H_path"))
 DEFAULT_PARAMS["M"], DEFAULT_PARAMS["H"] = M, H
 
-def set_parameters(input_params:dict[str, float|int|NDArray[FTYPE]|np.random.Generator]) -> RNASeqDeconvolution:
+def set_parameters(input_params: dict[str, float | int | NDArray[FTYPE] | np.random.Generator]) -> RNASeqDeconvolution:
     """
     Helper function which initializes the **many** variables across Population and Individual.
     Each variable-value pair in params has that value set for that variable.
@@ -61,6 +61,8 @@ def set_parameters(input_params:dict[str, float|int|NDArray[FTYPE]|np.random.Gen
     for key, value in input_params.items():
         p[key] = value
 
+    p["rng"] = np.random.default_rng(p["rng_seed"])
+
     # set static Individual parameters
     Individual.set_static_vars(p["rng"], p["mut_prob"], p["mut_standard_deviation"],
                                p["crossover_prob"], p["M"], p["H"])
@@ -70,9 +72,10 @@ def set_parameters(input_params:dict[str, float|int|NDArray[FTYPE]|np.random.Gen
     # return RNASeqDeconvolution initialized with pop
     return RNASeqDeconvolution(pop)
 
-def compare(var1:str, start1:float, step1:float, end1:float,
-            var2:str|None = None, start2:float|None = None, step2:float|None = None, end2:float|None = None) \
-            -> list[pd.DataFrame]:
+
+def compare(var1: str, start1: float, step1: float, end1: float,
+            var2: str | None = None, start2: float | None = None, step2: float | None = None, end2: float | None = None) \
+        -> list[pd.DataFrame]:
     """
     Generate a comparison matrix for different values of one or two variables.
     :param var1: name of variable to change
@@ -98,17 +101,17 @@ def compare(var1:str, start1:float, step1:float, end1:float,
         var1_values.append(var1_values[-1] + step1)
         var1_labels.append(var1 + " = " + str(var1_values[-1]))
     # if var2 isn't specified
-    if var2 is None: # only testing var1, everything else is as default
+    if var2 is None:  # only testing var1, everything else is as default
         result_matrix = []
         pbar = tqdm(range(len(var1_values)), total=len(var1_values), dynamic_ncols=True, position=0, leave=False)
-        for i in pbar: # for each value
+        for i in pbar:  # for each value
             pbar.set_postfix_str(var1_labels[i])
-            experiment = set_parameters({var1: var1_values[i]}) # set experiment to use current var1 value
-            experiment.run(False, 1) # run experiment
+            experiment = set_parameters({var1: var1_values[i]})  # set experiment to use current var1 value
+            experiment.run(False, 1)  # run experiment
             result_matrix += [[experiment.result_fitness_score, experiment.pop.current_iter,
-                               experiment.detect_stop_reason()]] #extract fitness score, iteration and stop reason
-        output = pd.DataFrame(result_matrix, var1_labels, RESULT_LABELS) # wrap in pandas DataFrame
-        return [output] # wrap output in list of size 1
+                               experiment.detect_stop_reason()]]  # extract fitness score, iteration and stop reason
+        output = pd.DataFrame(result_matrix, var1_labels, RESULT_LABELS)  # wrap in pandas DataFrame
+        return [output]  # wrap output in list of size 1
 
     # if var2 IS specified
     # initialize var2 values and labels
@@ -123,11 +126,11 @@ def compare(var1:str, start1:float, step1:float, end1:float,
     # testing all (var1 x var2) pairs
     pbar1 = trange(len(var1_values), total=len(var1_values), dynamic_ncols=True, position=0, leave=False)
     pbar2 = trange(len(var2_values), total=len(var2_values), dynamic_ncols=True, position=1, leave=False)
-    for i in pbar1: # for each var1 value
+    for i in pbar1:  # for each var1 value
         pbar1.set_postfix_str(var1_labels[i])
         # initialize result rows
         fitness_row, iteration_row, stop_reason_row = [], [], []
-        for j in pbar2: # for each var2 value
+        for j in pbar2:  # for each var2 value
             pbar2.set_postfix_str(var2_labels[j])
             # run experiment
             experiment = set_parameters({var1: var1_values[i], var2: var2_values[j]})
@@ -147,8 +150,32 @@ def compare(var1:str, start1:float, step1:float, end1:float,
     stop_reason_df = pd.DataFrame(stop_reason_matrix, var1_labels, var2_labels)
     return list([fitness_df, iteration_df, stop_reason_df])
 
-def get_true_results() -> NDArray[FTYPE]:
-    return parse_input_matrix(DEFAULT_PARAMS.get("true_result_path")).transpose()
+
+# def get_true_results() -> NDArray[FTYPE]:
+#     return parse_input_matrix(DEFAULT_PARAMS.get("true_result_path")).transpose()
+
+def get_true_results() -> pd.DataFrame:
+    df = pd.read_csv(DEFAULT_PARAMS["true_result_path"], sep="\t", index_col=0, header=0)
+    return df
+
+def compare_to_true_results(phen:NDArray[FTYPE]) -> pd.DataFrame:
+    """
+    Produce a DataFrame matrix of difference between result we got and the true result
+    :param phen: numpy matrix of result phenotype
+    :return: pandas DataFrame of difference when subtracting true result from result we got.
+             labeled in the same way as the true results
+    """
+    result = phen.transpose()
+    true_result_df = get_true_results()
+    # calculate difference between phenotype we got and true phenotype
+    true_result_phenotype = true_result_df.apply(pd.to_numeric, errors="coerce").to_numpy(dtype=FTYPE)
+    diff = result - true_result_phenotype
+    # extract labels from true_result_df
+    col_labels = true_result_df.columns.to_list()
+    row_labels = true_result_df.index.to_list()
+    # produce new DataFrame object for diff
+    df_diff = pd.DataFrame(diff, columns=col_labels, index=row_labels)
+    return df_diff
 
 if __name__ == "__main__":
     params = dict()
@@ -156,13 +183,8 @@ if __name__ == "__main__":
     experiment.run()
     result_phen = experiment.result
     result_fitness = experiment.result_fitness_score
-    true_phen = get_true_results()
-
     print("result phen:")
     print(pd.DataFrame(result_phen))
     print("result fitness score: " + str(result_fitness))
     print("difference from true results:")
-    print(pd.DataFrame(result_phen - true_phen))
-
-
-
+    print(compare_to_true_results(result_phen))
