31f8837563ba7769942645087e5541aeb4e086cb
3-kids-wearing-a-trenchcoat <d.spector1996@gmail.com>
2025-12-15 15:43:03 +0200

    Added min_mean to specificity matrix

--- maman12-q2/Markers.py
+++ maman12-q2/Markers.py
@@ -5,36 +5,35 @@ from numpy.typing import NDArray
 
 FTYPE = np.float64
 
-def gene_variance_and_mean(H_input: NDArray[FTYPE], log_transform:bool = True) -> tuple[NDArray[FTYPE], NDArray[FTYPE]]:
-    """return two numpy vectors.
-    The first representing variance of each gene (row), and the second
-    representing the mean of each gene."""
+def specificity_matrix(H_input: NDArray[FTYPE], min_mean:float, log_transform: bool = True) -> NDArray[FTYPE]:
+    """
+    Calculate a specificity score for every gene x celltype combination.
+    The specificity score of gene g for cell-type k is H's value at (g,k) divided by the sum of values
+    for gene g in all cell-types."""
     if log_transform:
-        H = np.log1p(H_input) # log(1+H)
+        H = np.log1p(H_input)  # log(1+H)
     else:
         H = H_input
-    return np.var(H, axis=1), np.mean(H, axis=1)
 
-def marker_genes_by_variance(H_input:NDArray[FTYPE], top_n:int = 200, min_mean:float = 1e-6,
-                             log_transform:bool = True) -> NDArray[bool]:
-    """
-    Generate a boolean vector indicating which genes are marker genes.
-    Marker genes are the genes whose variance across cell-types is the highest.
-    :param H_input: numpy matrix representing genes x cell-type
-    :param top_n: number of marker genes to select
-    :param min_mean: genes with a cell-type mean lower than this value will be disregarded as noise
-    :param log_transform: whether to apply log transform on H before choosing variances
-                            (reduces the dominance of high absolute values)
-    :return: boolean column vector where element i is True iff gene i (row i in H) is selected as a marker gene
-    """
-    H_var, H_mean = gene_variance_and_mean(H_input, log_transform)
-    H_var = np.where(H_mean < min_mean, 0, H_var)  # remove from consideration genes whose min is below minimum
-    if top_n > np.count_nonzero(H_var):  # if less than top_n values are left
-        return np.where(H_var > 0, True, False).astype(np.bool)  # all non-zero variables will be marked as marker genes
-    sorted_indexes = np.argsort(H_var)  # sort indexes by var in ascending order
-    top_indexes = sorted_indexes[-top_n:]  # select indexes of top_n highest var values
-    output = np.zeros(H_var.shape, dtype=np.bool)
-    output[top_indexes] = True  # value i is true iff the var of gene i is among the top_n highest
+    gene_sum = H.sum(axis=1, keepdims=True) + np.finfo(FTYPE).tiny
+    output = H / gene_sum # specificity matrix
+
+    # find genes whose mean is below the minimum and set their specificity to be very small,
+    # as these genes are probably noise and should only be used as a last resort
+    means = np.mean(H, axis=1, keepdims=True)
+    means = np.tile(means, output.shape[1])
+    eps = 1e-12
+    output = np.where(means < min_mean, eps, output)
+    return output
+
+def marker_genes_by_specificity(H_input:NDArray[FTYPE], k_per_cell:int = 30, min_mean:float = 1e-6,
+                                log_transform:bool = True) -> NDArray[bool]:
+    specif = specificity_matrix(H_input, min_mean, log_transform)
+    output = np.zeros(H_input.shape[0], dtype=bool)
+    for k in range(H_input.shape[1]):
+        scores = specif[:, k]  # (G,)
+        top_idx = np.argsort(scores)[-k_per_cell:]  # top k genes for this cell
+        output[top_idx] = True
     return output
 
 def redact_to_marker_genes(H_input:NDArray[FTYPE], M_input:NDArray[FTYPE],
@@ -50,6 +49,6 @@ def redact_to_marker_genes(H_input:NDArray[FTYPE], M_input:NDArray[FTYPE],
     :return: a tuple of new H and new M matrices with non-marker genes redacted
     """
     H, M = H_input.copy(), M_input.copy()
-    gene_marker_mask = marker_genes_by_variance(H, top_n, min_mean, log_transform)
+    gene_marker_mask = marker_genes_by_specificity(H, top_n, min_mean, log_transform)
     H, M = H[gene_marker_mask], M[gene_marker_mask]
     return H, M
