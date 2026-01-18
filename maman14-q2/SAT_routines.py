95669c7c6a007d8fb14d02b5ef259a3c1af4857a
3-kids-wearing-a-trenchcoat <d.spector1996@gmail.com>
2026-01-18 11:00:30 +0200

    implemented check_and_reindex

---
+++ maman14-q2/SAT_routines.py
@@ -0,0 +1,16 @@
+from __future__ import annotations
+import binding, strand, anneal, unravel, magnetic, SATinit
+from SATinit import variable_rep_false, variable_rep_true, connector_rep, complement_rep
+from TwoBitArray import TwoBitArray
+from typing import List, Tuple
+from tqdm import tqdm, trange
+
+
+def check_and_reindex(threshold:float) -> None:
+    """Check if there's a need to compactify the data, if so, reindex."""
+    reindex_binding, reindex_strand = binding.get_dead_fraction() > threshold, strand.get_dead_fraction() > threshold
+    # if strands above threshold, reindex and get the new IDs
+    new_ids = strand.reindex() if reindex_strand else None
+    if reindex_binding or reindex_strand:   # if bindings are above threshold OR strands were reindexed
+        binding.reindex(new_ids)            # Will also update IDs if strand was reindexed
+
