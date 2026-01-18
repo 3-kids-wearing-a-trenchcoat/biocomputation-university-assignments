bbf83b493b6fb8312b9dfd42659480fa6178d4a6
3-kids-wearing-a-trenchcoat <d.spector1996@gmail.com>
2026-01-18 11:02:51 +0200

    strand reindexing triggered either by strand dead fraction or strand dead fraction

--- maman14-q2/SAT_routines.py
+++ maman14-q2/SAT_routines.py
@@ -1,5 +1,5 @@
 from __future__ import annotations
-import binding, strand, anneal, unravel, magnetic, SATinit
+import binding, strand, anneal, unravel, magnetic, SATinit, sequences
 from SATinit import variable_rep_false, variable_rep_true, connector_rep, complement_rep
 from TwoBitArray import TwoBitArray
 from typing import List, Tuple
@@ -8,7 +8,9 @@ from tqdm import tqdm, trange
 
 def check_and_reindex(threshold:float) -> None:
     """Check if there's a need to compactify the data, if so, reindex."""
-    reindex_binding, reindex_strand = binding.get_dead_fraction() > threshold, strand.get_dead_fraction() > threshold
+    reindex_binding= binding.get_dead_fraction() > threshold
+    # reindex strands (and compactify sequences) if either strands or sequences are above threshold
+    reindex_strand = strand.get_dead_fraction() > threshold or sequences.get_dead_fraction() > threshold
     # if strands above threshold, reindex and get the new IDs
     new_ids = strand.reindex() if reindex_strand else None
     if reindex_binding or reindex_strand:   # if bindings are above threshold OR strands were reindexed
