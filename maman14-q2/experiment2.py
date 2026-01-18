a9a56be95802a4586f9ca40543fa23e716acb7f7
3-kids-wearing-a-trenchcoat <d.spector1996@gmail.com>
2026-01-18 20:49:53 +0200

    Added experiment2.py

---
+++ maman14-q2/experiment2.py
@@ -0,0 +1,15 @@
+"""This experiment is for an easily satisfiable formula of 4 variables"""
+
+from SAT_experiment import run
+
+# (x v y v z) ^ (x' v y v z) ^ (x v y' v z)
+formula = [(    (0, True), (1, True), (2, True) ),
+                ((0, False), (1, True), (3, True)),
+                ((0, True), (1, False), (3, True))]
+
+if __name__ == "__main__":
+    satisfiable = run(formula)
+    if satisfiable:
+        print("FORMULA IS SATISFIABLE")
+    else:
+        print("FORMULA IS NOT SATISFIABLE")
