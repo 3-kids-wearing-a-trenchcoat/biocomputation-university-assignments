186985e017e6b3420bb1d9f390e1db3319d608a5
3-kids-wearing-a-trenchcoat <d.spector1996@gmail.com>
2026-01-22 12:11:57 +0200

    Defined match between words in language to 5-bit strings

---
+++ maman15-q3/transcode.py
@@ -0,0 +1,25 @@
+"""This module contains the functions that convert between any of the following
+1. binary representation
+2. language representation (two-letter words in a 6-letter language)
+3. DNA representation"""
+
+from __future__ import annotations
+from typing import List, Tuple, TypeAlias
+import numpy as np
+from numpy.typing import NDArray
+
+# representations
+five_bit: TypeAlias = tuple[int, int, int, int, int]
+word_bit_pairs: List[Tuple[str, five_bit]] = [("UU", (0,0,0,0,0)), ("UV", (0,0,0,0,1)), ("UW", (0,0,0,1,0)),
+                                              ("UX", (0,0,0,1,1)), ("UY", (0,0,1,0,0)), ("UZ", (0,0,1,0,1)),
+                                              ("VU", (0,0,1,1,0)), ("VV", (0,0,1,1,1)), ("VW", (0,1,0,0,0)),
+                                              ("VX", (0,1,0,0,1)), ("VY", (0,1,0,1,0)), ("VZ", (0,1,0,1,1)),
+                                              ("WU", (0,1,1,0,0)), ("WV", (0,1,1,0,1)), ("WW", (0,1,1,1,0)),
+                                              ("WX", (0,1,1,1,1)), ("WY", (1,0,0,0,0)), ("WZ", (1,0,0,0,1)),
+                                              ("XU", (1,0,0,1,0)), ("XV", (1,0,0,1,1)), ("XW", (1,0,1,0,0)),
+                                              ("XX", (1,0,1,0,1)), ("XY", (1,0,1,1,0)), ("XZ", (1,0,1,1,1)),
+                                              ("YU", (1,1,0,0,0)), ("YV", (1,1,0,0,1)), ("YW", (1,1,0,1,0)),
+                                              ("YX", (1,1,0,1,1)), ("YY", (1,1,1,0,0)), ("YZ", (1,1,1,0,1)),
+                                              ("ZU", (1,1,1,1,0)), ("ZV", (1,1,1,1,1))]
+
+# dictionaries - matching between representations
